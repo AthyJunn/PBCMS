baae6a4f48b386d0492c863e4c0e5f69ffb2e5af
Athy <athyjunn@gmail.com>
2025-08-11 20:43:23 +0800

    CREATE CHANGE PASSWORD XML

---
+++ PBCMS/app/src/main/java/com/example/pbcms/ChangePwdActivity.java
@@ -0,0 +1,24 @@
+package com.example.template;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChangePwdActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_change_pwd);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
