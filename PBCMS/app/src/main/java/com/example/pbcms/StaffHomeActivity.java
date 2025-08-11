1666d675a34dd6412c91b330c15033fa7b0aee26
TanusshKumar <tanussh.kumar@graduate.utm.my>
2025-08-11 20:19:11 +0800

    REFACTOR STAFF HOMEPAGE & CREATE ADMIN HOMEPAGE

--- PBCMS/app/src/main/java/com/example/pbcms/StaffHomeActivity.java
+++ PBCMS/app/src/main/java/com/example/pbcms/StaffHomeActivity.java
@@ -1,20 +1,15 @@
 package com.example.pbcms;
 
-import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
-public class HomepageActivity extends AppCompatActivity {
+public class StaffHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_staff_home);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide(); // Hide the default action bar
