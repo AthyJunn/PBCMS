0d8d1f23770af51f15fd6e758da70b0afe7d7009
TanusshKumar <tanussh.kumar@graduate.utm.my>
2025-08-19 00:06:51 +0800

    CREATE VIEW STAFF XML

---
+++ PBCMS/app/src/main/java/com/example/pbcms/ViewStaffActivity.java
@@ -0,0 +1,13 @@
+package com.example.pbcms;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewStaffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_staff);
+    }
+}
